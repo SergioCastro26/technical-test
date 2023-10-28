7eb601d7ac85d43146e0e06b3ba548d0649da980
sergio.castro <sergio.castro@inti.solutions>
2023-10-28 03:34:20 -0500

    feature/bill_implement Se crea el BillRepository y se agregan metodos para el UserRepository

--- src/main/java/com/microservice/technicaltest/repository/UserRepository.java
+++ src/main/java/com/microservice/technicaltest/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.microservice.technicaltest.repository;
 
-import com.microservice.technicaltest.entity.UserEntity;
+import com.microservice.technicaltest.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findAll();
+    Optional<User> findById(Long idUser);
 }
