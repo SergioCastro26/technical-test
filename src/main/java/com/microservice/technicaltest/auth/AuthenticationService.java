a8da6fd3cc3130f97dca24e99e5c2aab012ebfb4
sergio.castro <sergio.castro@inti.solutions>
2023-10-28 08:58:26 -0500

    feature/jwt_implementation Se agregan archivos de configuracion security

---
+++ src/main/java/com/microservice/technicaltest/auth/AuthenticationService.java
@@ -0,0 +1,56 @@
+package com.microservice.technicaltest.auth;
+
+import com.microservice.technicaltest.model.Role;
+import com.microservice.technicaltest.model.User;
+import com.microservice.technicaltest.repository.UserRepository;
+import com.microservice.technicaltest.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private JwtService jwtService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setRole(Role.USER);
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder().
+                token(jwtToken).
+                build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder().
+                token(jwtToken).
+                build();
+    }
+}
---
+++ src/main/java/com/microservice/technicaltest/model/Role.java
@@ -0,0 +1,6 @@
+package com.microservice.technicaltest.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
